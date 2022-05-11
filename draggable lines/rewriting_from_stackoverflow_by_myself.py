1125243191a85dd0bd2a9d5247776f958ec0cdd8
Marcel Drąg <marcel.drag@interia.pl>
2022-05-11 18:38:03 +0200

    Made some changes in 'rewriting_from_stackoverflow_by_myself.py'

--- draggable lines/rewriting_from_stackoverflow_by_myself.py
+++ draggable lines/rewriting_from_stackoverflow_by_myself.py
@@ -3,14 +3,14 @@ import matplotlib.lines as lines
 
 switch = False
 last_click_x_cord = 0
+markers = []
 
 fig, ax = plt.subplots()
 ax.plot()
 default_xlim = ax.get_xlim()
 
 
-
-def dbl_lmb_to_create_rmb_to_delete_line(event):
+def dbl_lmb_to_create_line(event):
     global last_click_x_cord
     global make_the_line_follow_mouse_connector
 
@@ -18,13 +18,14 @@ def dbl_lmb_to_create_rmb_to_delete_line(event):
         line = ax.axvline(event.xdata, picker = True, pickradius = 5)
     elif event.button == plt.MouseButton.LEFT and event.dblclick == False:
         make_the_line_follow_mouse_connector = fig.canvas.mpl_connect('motion_notify_event', make_the_line_follow_mouse)
+    # print(event.xdata)
     # elif event.button == plt.MouseButton.RIGHT:
     #     clicked_object_path.remove()
 
     last_click_x_cord = event.xdata
 
 
-def get_path_to_clicked_object(event):
+def get_path_to_clicked_object_or_delete_it(event):
     global clicked_object_path
     global switch
 
@@ -38,7 +39,7 @@ def get_path_to_clicked_object(event):
 def make_the_line_follow_mouse(event):
     global clicked_object_path
     if switch == True:
-        clicked_object_path.set_xdata(event.xdata)
+        clicked_object_path.set_xdata([event.xdata, event.xdata])
 
 
 def stop_following_mouse_after_button_release(event):
@@ -47,25 +48,20 @@ def stop_following_mouse_after_button_release(event):
 
 def getting_list_of_vlines_x_cords():
     global sorted_vlines_x_cord
+    global markers
     list_of_lines = ax.get_lines()
-    vlines_x_cord_list = []
-
     for i in range(0,len(list_of_lines)):
-        if type(list_of_lines[i].get_xdata()) == type(vlines_x_cord_list):
-            vlines_x_cord_list.append(list_of_lines[i].get_xdata()[0])
-        else:
-            vlines_x_cord_list.append(list_of_lines[i].get_xdata())
-
-    vlines_x_cord_list.sort()
-    return vlines_x_cord_list
+        markers.append(int(list_of_lines[i].get_xdata()[0]))
+        markers.sort()
+    return markers
 
 
-
-lmb_to_create_line_connector = fig.canvas.mpl_connect('button_press_event', dbl_lmb_to_create_rmb_to_delete_line)
-click_on_line_connector = fig.canvas.mpl_connect('pick_event', get_path_to_clicked_object)
+lmb_to_create_line_connector = fig.canvas.mpl_connect('button_press_event', dbl_lmb_to_create_line)
+click_on_line_connector = fig.canvas.mpl_connect('pick_event', get_path_to_clicked_object_or_delete_it)
 stop_following_mouse_after_button_release_connector = fig.canvas.mpl_connect('button_release_event',stop_following_mouse_after_button_release)
 
 while True:
     plt.pause(0.05)
     if last_click_x_cord > default_xlim[1]*0.9:
         break
+print(getting_list_of_vlines_x_cords())
